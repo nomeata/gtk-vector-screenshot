d7cc778e130195c00733a1efb06152fd26760350
Joachim Breitner <mail@joachim-breitner.de>
2011-06-20 15:01:48 +0200

    Autotoolize

--- Makefile
+++
@@ -1,8 +0,0 @@
-all: libpdfscreenshot.so module.so
-
-libpdfscreenshot.so: pdfscreenshot.c
-	gcc -shared -fPIC -o libpdfscreenshot.so pdfscreenshot.c -ldl `pkg-config --cflags gdk-2.0`
-
-
-module.so: module.c
-	gcc -std=gnu99 -fPIC -shared $$(pkg-config --cflags --libs gtk+-3.0) $< -o $@
--- gtk-vector-screenshot.c
+++ gtk-vector-screenshot.c
@@ -1,3 +1,21 @@
+/*
+ * © 2011 Joachim Breitner <mail@joachim-breitner.de>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 
 // For strchrnul
 #define _GNU_SOURCE
@@ -162,6 +180,7 @@ pdfscreenshot_take_shot (GtkButton *button, gpointer our_window) {
             return;
         }
     }
+    g_list_free(toplevels);
 
     GtkWidget *dialog = gtk_message_dialog_new (our_window,
          GTK_DIALOG_DESTROY_WITH_PARENT,
